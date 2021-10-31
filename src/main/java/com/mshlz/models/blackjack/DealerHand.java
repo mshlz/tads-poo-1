a28105fea62d008b99dcc2e949ffc2ba674b5ccd
Mateus Holzschuh <mateusholzschuh@hotmail.com>
2021-10-31 13:34:19 -0300

    remove extra bracket from dealer preview

--- src/main/java/com/mshlz/models/blackjack/DealerHand.java
+++ src/main/java/com/mshlz/models/blackjack/DealerHand.java
@@ -3,7 +3,7 @@ package com.mshlz.models.blackjack;
 public class DealerHand extends PlayerHand {
 
     public String getPreviewString(Boolean showAll) {
-        return showAll ? "Dealer " + this.cards + "] (" + this.getHandValue() + " pontos)" : this.getPreviewString();
+        return showAll ? "Dealer " + this.cards + " (" + this.getHandValue() + " pontos)" : this.getPreviewString();
     }
 
     public String getPreviewString() {
