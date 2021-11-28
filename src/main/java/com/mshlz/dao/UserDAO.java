90d63113a0e1a880b74f61525edc794d556ea062
Mateus Holzschuh <mateusholzschuh@hotmail.com>
2021-11-28 16:42:03 -0300

    update user dao (return generated id when save)

--- src/main/java/com/mshlz/dao/UserDAO.java
+++ src/main/java/com/mshlz/dao/UserDAO.java
@@ -3,6 +3,7 @@ package com.mshlz.dao;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import com.mshlz.models.User;
 
@@ -12,15 +13,28 @@ public class UserDAO extends BaseDAO<User> {
     public Boolean save(User user) {
         try {
             String sql = "INSERT INTO public.user (name, nickname) VALUES (?,?)";
-            PreparedStatement statement = connection.prepareStatement(sql);
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
             statement.setString(1, user.getName());
             statement.setString(2, user.getNickname());
-            statement.execute();
+            int affectedRows = statement.executeUpdate();
 
+            if (affectedRows == 0) {
+                throw new SQLException("Creating user failed, no rows affected.");
+            }
+
+            ResultSet generatedKeys = statement.getGeneratedKeys();
+            
+            if (!generatedKeys.next()) {
+                throw new SQLException("Creating user failed, no id generated.");
+            }
+            
+            user.setId(generatedKeys.getLong(1));
+            
             connection.commit();
 
             return true;
+
         } catch (Exception e) {
             try {
                 connection.rollback();
