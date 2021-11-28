48ab6a3b0ba797f145c284b098ffc27f05ea7a2b
Mateus Holzschuh <mateusholzschuh@hotmail.com>
2021-11-28 17:34:03 -0300

    add match object (to save history in db)

---
+++ src/main/java/com/mshlz/models/Match.java
@@ -0,0 +1,72 @@
+package com.mshlz.models;
+
+import java.util.Date;
+
+import com.mshlz.models.blackjack.DealerHand;
+import com.mshlz.models.blackjack.PlayerHand;
+
+public class Match {
+    private Long id;
+    private User user;
+    private DealerHand dealerHand;
+    private PlayerHand playerHand;
+    private String result;
+    private Date date;
+
+    public Match() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public DealerHand getDealerHand() {
+        return dealerHand;
+    }
+
+    public void setDealerHand(DealerHand dealerHand) {
+        this.dealerHand = dealerHand;
+    }
+
+    public PlayerHand getPlayerHand() {
+        return playerHand;
+    }
+
+    public void setPlayerHand(PlayerHand playerHand) {
+        this.playerHand = playerHand;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Match [date=" + date + ", dealerHand=" + dealerHand + ", id=" + id + ", playerHand=" + playerHand
+                + ", result=" + result + ", user=" + user + "]";
+    }    
+}
